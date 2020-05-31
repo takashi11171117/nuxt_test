3183152bc95cbd479b0c4190e73bd2bf1e837288
Takashi Saito <takashi11171117@gmail.com>
2020-05-31 21:57:12 +0900

    add circle

--- components/P5/ts/PolarGrids.ts
+++ components/P5/ts/PolarGrids.ts
@@ -1,11 +1,18 @@
 import P5 from 'p5'
 
 const sketch = (p: P5) => {
+  // const radius: number, slices: number, slice: number
+  let radius: number
+  let x: number, y: number
+  // const circles: number
+
   p.preload = () => {}
 
   p.setup = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
     p.background(20, 50, 100)
+
+    radius = 100
 
     p.noStroke()
     p.noLoop()
@@ -13,9 +20,15 @@ const sketch = (p: P5) => {
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
+    p.background(20, 50, 100)
   }
 
-  p.draw = () => {}
+  p.draw = () => {
+    const angle = 0
+    x = p.windowWidth / 2 + p.cos(angle) * radius
+    y = p.windowHeight / 2 + p.sin(angle) * radius
+    p.ellipse(x, y, 8, 8)
+  }
 }
 
 export default sketch
