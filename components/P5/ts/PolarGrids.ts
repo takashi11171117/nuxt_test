210e17dd4785b4dc5a013ad3e349f2d6dbf70190
Takashi Saito <takashi11171117@gmail.com>
2020-06-25 22:13:18 +0900

    add circles more

--- components/P5/ts/PolarGrids.ts
+++ components/P5/ts/PolarGrids.ts
@@ -1,10 +1,9 @@
 import P5 from 'p5'
 
 const sketch = (p: P5) => {
-  // const radius: number, slices: number, slice: number
   let radius: number, slices: number, slice: number
   let x: number, y: number
-  // const circles: number
+  let circles: number
 
   p.preload = () => {}
 
@@ -13,7 +12,8 @@ const sketch = (p: P5) => {
     p.background(20, 50, 100)
 
     slices = 6
-    radius = 100
+    radius = 10
+    circles = 20
 
     p.noStroke()
     p.noLoop()
@@ -25,12 +25,15 @@ const sketch = (p: P5) => {
   }
 
   p.draw = () => {
-    for (let i = 0; i < slices; i++) {
-      slice = p.TWO_PI / slices
-      const angle = i * slice
-      x = p.windowWidth / 2 + p.cos(angle) * radius
-      y = p.windowHeight / 2 + p.sin(angle) * radius
-      p.ellipse(x, y, 8, 8)
+    for (let j = 0; j < circles; j++) {
+      for (let i = 0; i < slices; i++) {
+        slice = p.TWO_PI / slices
+        const angle = i * slice
+        x = p.windowWidth / 2 + p.cos(angle) * radius * (j + 1)
+        y = p.windowHeight / 2 + p.sin(angle) * radius * (j + 1)
+        p.ellipse(x, y, 8, 8)
+      }
+      slices += 6
     }
   }
 }
